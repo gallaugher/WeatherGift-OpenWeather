87c5cbfd2ab004be15c2819e4b6460824dd62cee
John Gallaugher <john.gallaugher@bc.edu>
2020-03-01 10:47:12 -0500

    Ch. 7.2 WeatherGift LocationList Table View setup

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by John Gallaugher on 2/29/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
