ec70de38c4a0938772665898ee091ecdeb9dbd04
John Gallaugher <john.gallaugher@bc.edu>
2020-05-29 21:28:51 -0400

    After Ch. 7.15 - OpenWeather added daily weather in table view

---
+++ WeatherGift/DailyTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DailyTableViewCell.swift
+//  WeatherGift
+//
+//  Created by John Gallaugher on 5/29/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailySummaryView: UITextView!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryView.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+        }
+    }
+}
