6e8a67a4ef3989ccdb1e42b1596866dc75387638
John Gallaugher <john.gallaugher@bc.edu>
2020-06-03 16:03:41 -0400

    Completed WeatherGift w/OpenWeatherMap API

---
+++ WeatherGift/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  WeatherGift
+//
+//  Created by John Gallaugher on 6/1/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
